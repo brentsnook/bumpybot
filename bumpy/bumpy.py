f90f2bc2778fcf5b6b928b8f746d33d0987b9506
Brent Snook <brent@fuglylogic.com>
2010-06-24 22:39:27 +1000

    insert time of feature run after feature name as inline blip

--- bumpy/bumpy.py
+++ bumpy/bumpy.py
@@ -3,6 +3,7 @@ import types
 import logging
 import re
 import os
+import urllib
 from waveapi import appengine_robot_runner
 from waveapi import robot
 from waveapi import events
@@ -25,7 +26,7 @@ def _setup_authentication(robot, wave_id):
 class Bumpy:
     
     ROBOT_URL = CONFIG['url']
-    FEATURE = re.compile('\W*Feature:(.*)')
+    FEATURE = re.compile("\W*Feature:(.*)\n")
     
     @classmethod
     def pull_url(self, wavelet):
@@ -43,7 +44,12 @@ class Bumpy:
 
     def push(self, url, content):
         wavelet = self._fetch_wavelet_from(url)
-        wavelet.reply("Some content came back:\n" + content)
+        results = content.split('=')[1]
+        features = json.loads(urllib.unquote(results))['features']
+        
+        for blip_id in features:
+            self._update_blip(wavelet.blips[blip_id], features[blip_id])
+ 
         self._robot.submit(wavelet)
 
     def run(self, onRobotAddedHandler):
@@ -58,7 +64,11 @@ class Bumpy:
             if match:
                 features[blip.blip_id] = {'name' : match.group(1), 'version' : str(blip.version), 'content' : blip.text}
         return json.dumps({'features' : features})
-    
+
+    def _update_blip(self, blip, feature):
+        end_of_feature_name = self.FEATURE.search(blip.text).end() - 2
+        blip.insert_inline_blip(end_of_feature_name).append('Last ran ' + feature['finished'])
+
     def _fetch_wavelet_from(self, url):
         wave_id = utils.extract_wave_id(url)
         wavelet_id = utils.generate_wavelet_id_from_wave_id(wave_id)
